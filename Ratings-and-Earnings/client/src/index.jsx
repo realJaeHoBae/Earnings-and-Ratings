549df22775ed9ff088c9bb830c8ea79a0baee25a
Jae <jaebae1111@gmail.com>
2018-07-20 10:18:27 -0700

    resolving conflicts

--- Ratings-and-Earnings/client/src/index.jsx
+++ Ratings-and-Earnings/client/src/index.jsx
@@ -1,2 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Data from '../../database/exampledata.js'
+
+import $ from 'jquery';
+console.log(data);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+    }
+
+  }
+
+  render () {
+    return (<div>
+      <p>ddkjojoij f</p>
+    </div>)
+  }
+
+  componentDidMount() {
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+console.log(Data);
